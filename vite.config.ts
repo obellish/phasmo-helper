d90753a7d4560ab16056b0f02e6232b2cb645657
obellish <obellish@mpilabels.com>
2025-04-03 08:27:25 -0400

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { visualizer } from 'rollup-plugin-visualizer';
+import sassDts from 'vite-plugin-sass-dts';
+
+const debug = true;
+
+// https://vite.dev/config/
+export default defineConfig({
+	css: {
+		modules: {
+			generateScopedName: debug ? '[name]_[local]_[hash:base64:5]' : '[hash:base64:5]',
+		},
+	},
+	build: {
+		minify: !debug ? 'esbuild' : false,
+		sourcemap: debug,
+		manifest: true,
+	},
+	clearScreen: false,
+	plugins: [
+		react({
+			jsxImportSource: '@welldone-software/why-did-you-render',
+		}),
+		sassDts({
+			enabledMode: ['development', 'production'],
+		}),
+		visualizer({
+			sourcemap: debug,
+		}),
+	],
+})
